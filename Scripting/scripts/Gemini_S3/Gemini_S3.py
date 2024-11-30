6072630f9b0871e18a6fbc1447dc78083c2ad3b4
JaveedYara72 <55245021+JaveedYara72@users.noreply.github.com>
2024-11-30 18:35:50 +0530

    Update Gemini_S3.py

--- Scripting/scripts/Gemini_S3/Gemini_S3.py
+++ Scripting/scripts/Gemini_S3/Gemini_S3.py
@@ -10,22 +10,22 @@ import pandas as pd
 from io import BytesIO
 
 
-# # wrapper for amazon aws s3
-# AWS_ACCESS_KEY_ID = sys.argv[1]
-# AWS_SECRET_ACCESS_KEY = sys.argv[2]
+# wrapper for amazon aws s3
+AWS_ACCESS_KEY_ID = sys.argv[1]
+AWS_SECRET_ACCESS_KEY = sys.argv[2]
 
-# directory = os.getcwd()
-
-
-# vBucket = 'una-brands-ops'
-# s3_client = boto3.client('s3',
-#         aws_access_key_id= AWS_ACCESS_KEY_ID,aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
-# s3_resource = boto3.resource('s3',
-#         aws_access_key_id= AWS_ACCESS_KEY_ID,aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
-# VInputRawPrefix = 'data/prod/landing_raw/inventory_scrapes/gemini/'
+directory = os.getcwd()
 
 
-spreadsheetId = "10jxi5VRPGIEmLFyLmBrcyRkxd4jFHJoFCOh2rGheKr4" # Please set your Spreadsheet ID.
+vBucket = 'Random-ops'
+s3_client = boto3.client('s3',
+        aws_access_key_id= AWS_ACCESS_KEY_ID,aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+s3_resource = boto3.resource('s3',
+        aws_access_key_id= AWS_ACCESS_KEY_ID,aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+VInputRawPrefix = 'xxxxxxxxx/inventory_scrapes/gemini/'
+
+
+spreadsheetId = "xxxxxxxxxxxx" # Please set your Spreadsheet ID.
 url = "https://docs.google.com/spreadsheets/export?exportFormat=xlsx&id=" + spreadsheetId
 res = requests.get(url)
 data = BytesIO(res.content)
@@ -40,13 +40,13 @@ for name in xlsx.sheetnames:
         # reading it into a dataframe
         values = pd.read_excel(data, sheet_name="Gemini Inventory",index_col=None)
         
-        # with io.StringIO() as csv_buffer:
-        #         values.to_csv(csv_buffer, index=False)
-        #         response = s3_client.put_object(
-        #                         Bucket=vBucket, Key='{}Gemini_Inventory-{}.csv'.format(VInputRawPrefix,vLatestUpdatedDateTimeFile),Body=csv_buffer.getvalue())
-        #         status = response.get("ResponseMetadata", {}).get("HTTPStatusCode")
+        with io.StringIO() as csv_buffer:
+                values.to_csv(csv_buffer, index=False)
+                response = s3_client.put_object(
+                                Bucket=vBucket, Key='{}Gemini_Inventory-{}.csv'.format(VInputRawPrefix,vLatestUpdatedDateTimeFile),Body=csv_buffer.getvalue())
+                status = response.get("ResponseMetadata", {}).get("HTTPStatusCode")
 
-        #         if status == 200:
-        #             print(f"Successful S3 put_object response. Status - {status}")
-        #         else:
-        #             print(f"Unsuccessful S3 put_object response. Status - {status}")
+                if status == 200:
+                    print(f"Successful S3 put_object response. Status - {status}")
+                else:
+                    print(f"Unsuccessful S3 put_object response. Status - {status}")
